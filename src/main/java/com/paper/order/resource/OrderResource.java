9c98cd2bbf803428a95c71e2656dc989a62afb63
nagendra462 <67103687+nagendra462@users.noreply.github.com>
2022-11-29 13:05:32 +0530

    short term fix for cors

--- src/main/java/com/paper/order/resource/OrderResource.java
+++ src/main/java/com/paper/order/resource/OrderResource.java
@@ -2,6 +2,7 @@ package com.paper.order.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,21 +23,24 @@ public class OrderResource {
 	@Autowired
 	private OrderService orderService;
 
+	@CrossOrigin(value = "http://localhost:3000")
 	@PostMapping("/createorder")
 	public ResponseEntity<?> createOrder(@RequestBody CreateOrderRequest request) {
 		return this.orderService.createOrder(request);
 	}
-	
+	@CrossOrigin(value = "http://localhost:3000")
 	@GetMapping("/getorders")
 	public ResponseEntity<?> getOrders(){
 		return this.orderService.getOrders();
 	}
 	
+	@CrossOrigin(value = "http://localhost:3000")
 	@PutMapping("/updateorder")
 	public ResponseEntity<?> updateOrder(@RequestBody UpdateOrderRequest request){
 		return this.orderService.updateOrder(request);
 	}
 	
+	@CrossOrigin(value = "http://localhost:3000")
 	@DeleteMapping("/deleteorder")
 	public ResponseEntity<?> deleteOrder(@RequestParam String orderId){
 		return this.orderService.deleteOrder(orderId);
